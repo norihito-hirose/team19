22df7462b0854305375dccef44d98851e6f26347
ima-s <shigeaki.imakiire1109@gmail.com>
2017-12-26 20:37:40 +0900

    changed file name

--- qrnn_code_prediction/network.py
+++ qrnn_code_prediction/network.py
@@ -52,3 +52,25 @@ class LSTMLayer(nn.Module):
                 H = torch.cat((H, h.unsqueeze(2)), dim=2)
 
         return H
+
+class Net(nn.Module):
+    def __init__(self, vocab_size, embedding_size=200):
+        super(Net, self).__init__()
+
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+
+        self.lstm = nn.Sequential(
+            LSTMLayer(embedding_size, 256),
+            LSTMLayer(256, 256),
+            LSTMLayer(256, 256)
+        )
+        self.fc = nn.Linear(256, vocab_size)
+
+    def forward(self, x):
+        out = self.embedding(x)
+        out = out.transpose(1, 2)
+        out = self.lstm(out)
+        out = out.transpose(1, 2)
+        out = self.fc(out)
+
+        return out
