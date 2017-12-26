6ab29ce73cc7cf8110bb1a9a0d8d9299f12950f1
ima-s <shigeaki.imakiire1109@gmail.com>
2017-12-26 20:37:56 +0900

    changed config

--- qrnn_code_prediction/utils.py
+++ qrnn_code_prediction/utils.py
@@ -1,3 +1,5 @@
+import torch
+import numpy as np
 import glob
 
 class DataPreprocessor(object):
@@ -13,7 +15,7 @@ class DataPreprocessor(object):
         seqs = []
         for f in files:
             seq = open(f).read().strip().split("\n")
-            if len(seq) < 600 and len(seq) > 2:
+            if len(seq) < 512 and len(seq) > 2:
                 seqs.append(seq)
         seqs.sort(key=len)
 
@@ -41,6 +43,9 @@ class DataPreprocessor(object):
 
         return indexed_seqs
 
+    def save_dict(self):
+        np.save()
+
 def prepare_batch(seqs, start_idx, batch_size):
     end_idx = start_idx + batch_size
     if end_idx > len(seqs):
@@ -55,3 +60,7 @@ def prepare_batch(seqs, start_idx, batch_size):
         seq += [0 for i in range(max_length - len(seq))]
 
     return batched_seq, lengths
+
+def save_model(Net, model_dir):
+    torch.save(Net.state_dict(), "%s/Net.pth" % (model_dir))
+    print("Save Model")
